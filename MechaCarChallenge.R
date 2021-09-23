2f5b6bb73f9c7a070cf457896ee7180a040a8ca7
weihaolun <haolun@hlwei.me>
2021-09-22 21:30:34 -1000

    relocate datasource

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,10 +2,11 @@
 library(tidyverse)
 
 # Import and read in the csv file as a data frame
-mecha_mpg <- read.csv(file='MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+mecha_mpg <- read.csv(file='Datasources/MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 
 # Linear regression using lm() function for all 6 variables.
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mecha_mpg)
 
 # Summary() to determine p-value and the r-squared value.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mecha_mpg))
+
