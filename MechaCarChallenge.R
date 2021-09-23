57e3a7019a3b980acc7597f4e07c6f8cb11b5e2a
weihaolun <haolun@hlwei.me>
2021-09-23 02:15:10 -1000

    update script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,22 @@ total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI
 # to group each manufacturing lot by the mean, median, variance and sd.
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
+
+# Deliverable 3 - T-Test on Suspension Coils
+# Use t.test() function to determine if the PSI across all manufacturing lots 
+# is statistically different from the population mean of 1,500 pounds per sq inch
+t.test(suspension_coil$PSI, mu=1500)
+
+# Write 3 more scripts using t.test() and its subset() argument to determine if the PSI for 
+# each manufacturing lot is statistically different from the
+# population mean of 1500 pounds per square inch.
+
+# For Lot1
+t.test((subset(suspension_coil, Manufacturing_Lot=='Lot1'))$PSI, mu=1500)
+
+# For Lot2
+t.test((subset(suspension_coil, Manufacturing_Lot=='Lot2'))$PSI, mu=1500)
+
+# For Lot3
+t.test((subset(suspension_coil, Manufacturing_Lot=='Lot3'))$PSI, mu=1500)
+
